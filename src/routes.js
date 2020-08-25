2dd988bda15e9b321ab86c39e6ab763da1f84fa3
Tatiana <trodrigu7@cuc.edu.co>
2020-08-25 16:15:43 -0500

    privates routes

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,66 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import {
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+  Switch,
+  useLocation
+} from "react-router-dom";
 
 import ErrorPage from "./ErrorPage";
-import Dash from "./pages/Dash";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
+
+const Dashboard = lazy(() => import("./pages/Dash"));
+
+const publicRoutes = [
+  {
+    path: "/",
+    exact: true,
+    component: lazy(() => import("./pages/Auth/Login"))
+  },
+  {
+    path: "/login",
+    component: lazy(() => import("./pages/Auth/Login"))
+  },
+  {
+    path: "/register",
+    component: lazy(() => import("./pages/Auth/Register"))
+  }
+];
+
+function PrivateRoute({ children, ...rest }) {
+  const location = useLocation();
+  const isLoggedIn = useSelector(state => state.Auth.uid);
+
+  if (isLoggedIn) return <Route {...rest}>{children}</Route>;
+
+  return (
+    <Redirect
+      to={{
+        pathname: "/login",
+        state: { from: location }
+      }}
+    />
+  );
+}
 
 export default function Routes() {
   return (
     <ErrorPage>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Dash} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Switch>
+            {publicRoutes.map((route, index) => (
+              <Route key={index} path={route.path} exact={route.exact}>
+                <route.component />
+              </Route>
+            ))}
+            <PrivateRoute path="/dash">
+              <Dashboard />
+            </PrivateRoute>
+          </Switch>
+        </Router>
+      </Suspense>
     </ErrorPage>
   );
 }
